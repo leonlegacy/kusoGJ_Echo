d0066bb321e7b559396f634976d2f93f919efdd4
rino3390 <sherikawaii3390@gmail.com>
2023-04-08 20:46:02 +0800

    feat: make audience up down loop

---
+++ Assets/Scripts/InGame/UpDownLoop.cs
@@ -0,0 +1,14 @@
+﻿using DG.Tweening;
+using System;
+using UnityEngine;
+
+namespace CliffLeeCL.InGame
+{
+	public class UpDownLoop: MonoBehaviour
+	{
+		private void Start()
+		{
+			transform.DOLocalMoveY(0.2f, 0.5f).SetLoops(-1, LoopType.Yoyo);
+		}
+	}
+}
