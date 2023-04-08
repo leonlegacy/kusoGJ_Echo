530ed691aec62f86d3ec6fb1e1b416a920944877
rino3390 <sherikawaii3390@gmail.com>
2023-04-08 20:27:35 +0800

    feat: set game bg

---
+++ Assets/Scripts/InGame/BackGroundControl.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CliffLeeCL.InGame
+{
+	public class BackGroundControl: MonoBehaviour
+	{
+		[SerializeField]
+		private SpriteRenderer background;
+
+		[SerializeField]
+		private List<Sprite> spriteList;
+
+		private int stage;
+		private void Awake()
+		{
+			EventManager.Instance.onPlayerScored += OnScoredChange;
+		}
+
+		private void Start()
+		{
+			stage = 0;
+		}
+
+		private void OnScoredChange()
+		{
+			var scoreManager = ScoreManager.Instance;
+
+			switch(stage)
+			{
+				case 0:
+				{
+					if(scoreManager.CurrentScore <= scoreManager.TotalScore / 3)
+					{
+						Debug.Log($"觀眾掰掰");
+						stage += 1;
+					}
+					break;
+				}
+				case 1:
+				{
+					if(scoreManager.CurrentScore <= scoreManager.TotalScore / 2)
+					{
+						background.sprite = spriteList[1];
+						stage += 1;
+					}
+					break;
+				}
+			}
+		}
+	}
+}
