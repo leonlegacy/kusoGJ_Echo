ea297a52d078964803472268467199b93b757da7
rino3390 <sherikawaii3390@gmail.com>
2023-04-08 17:48:41 +0800

    feat: create record scene

---
+++ Assets/Scripts/Rhythm/RecordBeatMap.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Rhythm
+{
+	public class RecordBeatMap: MonoBehaviour
+	{
+		[SerializeField]
+		private MusicDatas musicDatas;
+
+		[SerializeField]
+		private int index;
+
+		[SerializeField]
+		private Button recordBtn;
+
+		private MusicData musicData;
+		private bool isRecord;
+		private float lastTime;
+		private float lastCreateTime;
+
+		private void Start()
+		{
+			recordBtn.onClick.AddListener(Record);
+		}
+
+		private void Update()
+		{
+			if(!isRecord || musicData == null) return;
+
+			if(Input.anyKeyDown)
+			{
+				lastCreateTime = Time.time-lastTime + lastCreateTime;
+				musicData.beatMap.Add(new Beat(){createTime = lastCreateTime});
+				lastTime = Time.time;
+			}
+		}
+
+		public void Record()
+		{
+			if(!isRecord)
+			{
+				musicData = musicDatas.musics.Find(x => x.musicId == index);
+				musicData.beatMap = new List<Beat>();
+				isRecord = true;
+				lastTime = Time.time;
+				lastCreateTime = 0;
+				recordBtn.GetComponent<Image>().color = Color.red;
+			}
+			else
+			{
+				isRecord = false;
+				recordBtn.GetComponent<Image>().color = Color.white;
+			}
+		}
+	}
+}
