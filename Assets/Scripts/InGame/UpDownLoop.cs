b0186f1bb7b3d00a2c6cb27467bff0c58b760d24
Cliff Lee CL <tommyboys0107@gmail.com>
2023-04-09 10:52:58 +0800

    [Modify] Make audience move with song bpm.

--- Assets/Scripts/InGame/UpDownLoop.cs
+++ Assets/Scripts/InGame/UpDownLoop.cs
@@ -8,22 +8,24 @@ namespace CliffLeeCL.InGame
 {
 	public class UpDownLoop: MonoBehaviour
 	{
+		[SerializeField] Ease ease = Ease.InOutSine;
 		private TweenerCore<Vector3, Vector3, VectorOptions> tween;
 
 		private void Awake()
 		{
+			EventManager.Instance.onMusicPlay += StartAudience;
 			EventManager.Instance.onStopAudience += StopAudience;
+		}
+
+		void StartAudience(Song song)
+		{
+			tween = transform.DOLocalMoveY(0.2f, song.BeatPeriod).SetDelay(song.reversedBeatOffset).SetLoops(-1, LoopType.Yoyo).SetEase(ease);
 		}
 
 		private void StopAudience()
 		{
 			tween.Kill();
 			transform.DOLocalMoveY(-10, 0.5f);
-		}
-
-		private void Start()
-		{
-			tween = transform.DOLocalMoveY(0.2f, 0.5f).SetLoops(-1, LoopType.Yoyo);
 		}
 	}
 }
--- Assets/Scripts/Song.cs
+++ Assets/Scripts/Song.cs
@@ -6,9 +6,13 @@ using UnityEngine;
 [Serializable][CreateAssetMenu(fileName = "Song", menuName = "ScriptableObjects/Song", order = 1)]
 public class Song : SerializedScriptableObject
 {
+    public float BeatPeriod => 60f / beatsPerMinute;
+    
     public AudioManager.AudioName audioName;
     public string songName;
     public float duration;
+    public float forwardBeatOffset;
+    public float reversedBeatOffset;
     public int beatsPerMinute;
     public int songId;
 }
