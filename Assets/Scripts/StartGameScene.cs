bdaa00ab72ff36694901dc1a9e8a8f54ebf8d97b
rino3390 <sherikawaii3390@gmail.com>
2023-04-09 11:47:27 +0800

    feat: add start scene

---
+++ Assets/Scripts/StartGameScene.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace CliffLeeCL
+{
+	public class StartGameScene: MonoBehaviour
+	{
+		[SerializeField]
+		private Button startBtn;
+
+		private void Awake()
+		{
+			startBtn.onClick.AddListener(GameStart);
+		}
+
+		private void GameStart()
+		{
+			SceneManager.LoadScene("Game");
+		}
+	}
+}
