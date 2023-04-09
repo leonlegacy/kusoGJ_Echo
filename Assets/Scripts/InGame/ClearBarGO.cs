29c7dab5e9527a3af63c21fd75867ed4fb7621c1
rino3390 <sherikawaii3390@gmail.com>
2023-04-09 14:01:30 +0800

    feat: clear bar change with score

---
+++ Assets/Scripts/InGame/ClearBarGO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CliffLeeCL.InGame
+{
+	public class ClearBarGO: MonoBehaviour
+	{
+		[SerializeField]
+		private Image clearBar;
+
+		private void Awake()
+		{
+			EventManager.Instance.onPlayerScored += ScoreChanged;
+		}
+
+		private void ScoreChanged()
+		{
+			clearBar.fillAmount = ScoreManager.Instance.CurrentScore / ScoreManager.Instance.TotalScore;
+		}
+	}
+}
