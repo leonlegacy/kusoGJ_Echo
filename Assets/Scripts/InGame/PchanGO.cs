e1db85a5b56d284af1fb173b493d7af4ac09982d
rino3390 <sherikawaii3390@gmail.com>
2023-04-09 11:10:29 +0800

    feat: pchan listen to music play

--- Assets/Scripts/InGame/PchanGO.cs
+++ Assets/Scripts/InGame/PchanGO.cs
@@ -1,10 +1,24 @@
 ﻿using DG.Tweening;
+using System;
 using UnityEngine;
 
 namespace CliffLeeCL.InGame
 {
 	public class PchanGO: MonoBehaviour
 	{
+		[SerializeField]
+		private Animator animator;
+
+		private void Awake()
+		{
+			EventManager.Instance.onMusicPlay += PlayAni;
+		}
+
+		private void PlayAni(Song obj)
+		{
+			animator.Play("Pchan");
+		}
+
 		public void Leave()
 		{
 			transform.DOMoveY(-10, 0.5f).OnComplete(()=> gameObject.SetActive(false));
