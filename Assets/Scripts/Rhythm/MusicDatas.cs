db90f7cbfacc109d49746ae4c3a5155bb5cd487a
Cliff Lee CL <tommyboys0107@gmail.com>
2023-04-09 15:14:54 +0800

    [Modify] Adjust setting for windows ver.

--- Assets/Scripts/Rhythm/MusicDatas.cs
+++ Assets/Scripts/Rhythm/MusicDatas.cs
@@ -1,9 +1,6 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Sirenix.OdinInspector;
-using Sirenix.Utilities.Editor;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Rhythm{
 	[CreateAssetMenu(fileName = "MusicDatas",menuName = "Data/MusicDatas")]
