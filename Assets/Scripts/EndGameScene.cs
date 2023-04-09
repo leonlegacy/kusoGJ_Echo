f2e6df4d64e1292256f6728b6dad4460123f819a
rino3390 <sherikawaii3390@gmail.com>
2023-04-09 14:48:49 +0800

    feat: end scene

---
+++ Assets/Scripts/EndGameScene.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace CliffLeeCL
+{
+	public class EndGameScene: MonoBehaviour
+	{
+		[SerializeField]
+		private Button exitBtn;
+
+		[SerializeField]
+		private Button startBtn;
+
+		private void Awake()
+		{
+			exitBtn.onClick.AddListener(()=>ExitGame());
+			startBtn.onClick.AddListener(()=>StartGame());
+		}
+
+		private void StartGame()
+		{
+			SceneManager.LoadScene("Game");
+		}
+
+		private void ExitGame()
+		{
+			Application.Quit();
+		}
+	}
+}
